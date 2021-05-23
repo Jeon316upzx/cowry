f8e10a7db00d4e7e259220e357a86ea3ff6f42c0
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2021-05-23 20:32:22 +0100

    hdhdhd

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,8 @@
 '''
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 # Root Url
@@ -14,3 +16,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cowrywise/api/v1/', include('api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
